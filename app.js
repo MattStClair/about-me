20fe823d619e93eb50fc586eedc558dc3f8f404d
mstclair <stclairm@spu.edu>
2017-07-11 17:06:13 -0700

    I really appreciated the help from the TA's and I really really should use this process more often to add, commit, and push versions of things I am trying

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-alert('hey is this thing on?')
+'use strict';
+
+  var answer1 = prompt("Do I like dogs?").toUpperCase();
+  if (answer1 === "YES" || answer1 === "Y")
+    {
+       alert("you answered correctly!");
+
+    }
+  else
+    {
+      alert("you answered incorrect")
+    }
+
+    console.log("user answered "+answer1+ " as to whether I like dogs");
+
+  var answer2 = prompt("Do I beleive in the idea of aliens?").toUpperCase();
+  if (answer2 === "YES" || answer2 === "Y")
+    {
+       alert("you answered correctly!");
+
+    }
+  else
+    {
+      alert("you answered incorrect")
+
+    }
+      console.log("user answered "+answer2+ " as to whether I beleive in aliens");
+
+  var answer3 = prompt("Was I in the military?").toUpperCase();
+  if (answer3 === "YES" || answer3 === "Y")
+    {
+       alert("you answered correctly!");
+       console.log("user answered "+ answer3 + " as to whether I was in military");
+    }
+  else
+    {
+      alert("you answered incorrect")
+      console.log("user answered "+ answer3 + " as to whether I was in the military");
+    }
+  var answer4 = prompt("Can I probably hold my breath for a minute?").toUpperCase();
+  if (answer4 === "YES" || answer4 === "Y")
+    {
+       alert("you answered correctly!");
+       console.log("user answered "+ answer4 + " as to whether I can hold my breath for a minute");
+    }
+  else
+    {
+      alert("you answered incorrect")
+      console.log("user answered "+ answer4 + " as to whether I can hold my breath for a minute");
+    }
+  var answer5 = prompt("Do I like coding? ").toUpperCase();
+  if (answer5 === "YES" || answer5 === "Y")
+    {
+       alert("you answered correctly!");
+       console.log("user answered "+ answer5 + " as to whether I like coding");
+    }
+  else
+    {
+      alert("you answered incorrect")
+      console.log("user answered "+ answer5 + " as to whether I like coding");
+    }
+
+
+
+  // alert('Nice to meet you, ' + username + '. I have been to ' + origin + '. It is cool you like to ' + activity +' . If you do not mind, I am going with you to ' + partyLocation +
+  // '. Thanks for stoppping by.');
+  // console.log('The user said their name is: ' + username + '. origin is: ' + origin + '. fun activity is: ' + activity + '. party locale: ' + partyLocation + '.');
+  //  //contextualized message for you to check in java console//
