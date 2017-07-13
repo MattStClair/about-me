09498360420f4f872765214427d583e3a7ca3d39
mstclair <stclairm@spu.edu>
2017-07-13 07:14:50 -0700

    without console messages in q6 or q7

--- app.js
+++ app.js
@@ -1,97 +1,136 @@
 'use strict';
 
 
-  var correct = [];
+var correct = ['starter'];
 
-  var amtCorrect = correct.Length;
+var amtCorrect = correct.length;
 
-  var answer1 = prompt('Do I like dogs?').toUpperCase();
-  if (answer1 === 'YES' || answer1 === 'Y')
-    {
-       alert('you answered correctly!');
+var answer1 = prompt('Do I like dogs?').toUpperCase();
+if (answer1 === 'YES' || answer1 === 'Y')
+{
+  alert('you answered correctly!');
+  correct.push('Dogs');
+}
+else
+{
+  alert('you answered incorrect')
+}
 
-    }
-  else
-    {
-      alert('you answered incorrect')
-    }
+console.log('user answered '+answer1+ ' as to whether I like dogs');
 
-    console.log('user answered '+answer1+ ' as to whether I like dogs');
 
-  var answer2 = prompt('Do I beleive in the idea of aliens?').toUpperCase();
-  if (answer2 === 'YES' || answer2 === 'Y')
-    {
-       alert('you answered correctly!');
+//---question 2 ------------------
+var answer2 = prompt('Do I beleive in the idea of aliens?').toUpperCase();
+if (answer2 === 'YES' || answer2 === 'Y')
+{
+  alert('you answered correctly!');
+  correct.push('Aliens');
 
-    }
-  else
-    {
-      alert('you answered incorrect')
+}
+else
+{
+  alert('you answered incorrect')
 
-    }
-      console.log('user answered '+answer2+ ' as to whether I beleive in aliens');
+}
+console.log('user answered '+answer2+ ' as to whether I beleive in aliens');
 
-  var answer3 = prompt('Was I in the military?').toUpperCase();
-  if (answer3 === 'YES' || answer3 === 'Y')
-    {
-       alert('you answered correctly!');
-       console.log('user answered '+ answer3 + ' as to whether I was in military');
-    }
-  else
-    {
-      alert('you answered incorrect')
-      console.log('user answered '+ answer3 + ' as to whether I was in the military');
-    }
-  var answer4 = prompt('Can I probably hold my breath for a minute?').toUpperCase();
-  if (answer4 === 'YES' || answer4 === 'Y')
-    {
-       alert('you answered correctly!');
-       console.log('user answered '+ answer4 + ' as to whether I can hold my breath for a minute');
-    }
-  else
-    {
-      alert('you answered incorrect')
-      console.log('user answered '+ answer4 + ' as to whether I can hold my breath for a minute');
-    }
-  var answer5 = prompt('Do I like coding? ').toUpperCase();
-  if (answer5 === 'YES' || answer5 === 'Y')
-    {
-       alert('you answered correctly!');
-       console.log('user answered '+ answer5 + ' as to whether I like coding');
-    }
-  else
-    {
-      alert('you answered incorrect')
-      console.log('user answered '+ answer5 + ' as to whether I like coding');
-    }
+// -----question 3
 
-    // -------------------question 6--------------------------
+var answer3 = prompt('Was I in the military?').toUpperCase();
+if (answer3 === 'YES' || answer3 === 'Y')
+{
+  alert('you answered correctly!');
+  correct.push('yes military');
+  console.log('user answered '+ answer3 + ' as to whether I was in military');
+}
+else
+{
+  alert('you answered incorrect')
+  console.log('user answered '+ answer3 + ' as to whether I was in the military');
+}
 
-    for (var i = 0; i<4; i++)
-    {
-    var answer6 = parseInt(prompt('Try and guess what number I am thinking of between 1-100'));
-    if (answer6 === 4)
-      {
-         alert('you answered correctly!');
-         correct.push('yes');
-        console.log('user answered '+ answer6 + ' for number guesss');
-      }
-    else if (answer6 > 4)
-      {
-        alert('you answered too high, try again friend!')
-        console.log('user answered '+ answer6 + ' for number guess');
-      }
-    else (answer6 < 4)
-    {
-      alert('you answered too low, try again friend!')
-      console.log('user answered '+ answer6 + ' for number guess');
-    }
+// question 4-------------------------------------------------
+var answer4 = prompt('Can I probably hold my breath for a minute?').toUpperCase();
+if (answer4 === 'YES' || answer4 === 'Y')
+{
+  alert('you answered correctly!');
+  correct.push('yesBreath');
+  console.log('user answered '+ answer4 + ' as to whether I can hold my breath for a minute');
+}
+else
+{
+  alert('you answered incorrect')
+  console.log('user answered '+ answer4 + ' as to whether I can hold my breath for a minute');
+}
+
+// --------------------question 5 --------------------------
+
+var answer5 = prompt('Do I like coding? ').toUpperCase();
+if (answer5 === 'YES' || answer5 === 'Y')
+{
+  alert('you answered correctly!');
+  correct.push('yescode');
+  console.log('user answered '+ answer5 + ' as to whether I like coding');
+}
+else
+{
+  alert('you answered incorrect')
+  console.log('user answered '+ answer5 + ' as to whether I like coding');
+}
+
+//-------------------question 6--------------------------
+
+var counter = 4;
+while (counter > 0)
+{
+  var userResponse = parseInt(prompt('guess the number between 1-10'));
+  if(userResponse === 5)
+  {
+    alert('you are correct');
+    break;
+    correct.push('numbers');
   }
+  else if (userResponse > 5 )
+  {
+    alert('too high, you have ' + counter + ' guesses left');
+    counter -= 1;
+  }
+  else (userResponse < 5)
+  {
+    alert('too low ' + counter + ' guesses left');
+    counter -= 1;
+  }
+}
+//---seven---------------
+var states = ['MONTANA', 'CALIFORNIA'];
 
-      alert('you answered: ' + amtCorrect+ 'correct out of 7!');
+var stateGuessCounter = 1;
+while (stateGuessCounter < 6)
+{
+    var answer7 = prompt('Guess a state I have lived in besides WA?').toUpperCase();
+
+        for (var possibleAnswer = 0; possibleAnswer < 2; possibleAnswer++)
+              {
+                if (answer7 === states[0] || answer7 === states [1])
+                {
+                  alert('You answered correctly! I have lived in ' + answer7);
+                  correct.push('yesState');
+                  console.log('user answered '+ answer7 + ' for states');
+                  stateGuessCounter +=6;
+                  break;
+                }
+                else
+                {
+                  alert('you answered incorrect');
+                  console.log('user answered '+ answer7 + ' for states');
+                  stateGuessCounter++;
+                  break;
+                }
+
+              }
+            }
 
 
-  // alert('Nice to meet you, ' + username + '. I have been to ' + origin + '. It is cool you like to ' + activity +' . If you do not mind, I am going with you to ' + partyLocation +
-  // '. Thanks for stoppping by.');
-  // console.log('The user said their name is: ' + username + '. origin is: ' + origin + '. fun activity is: ' + activity + '. party locale: ' + partyLocation + '.');
-  //  //contextualized message for you to check in java console//
+
+
+alert('you answered: ' + correct.length + ' correct out of 7!');
